41fb8f6e37c9e96d2580360f7657f4066ed17655
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2020-04-26 19:50:07 -0400

    - add test case

--- tests/test_stop_watch.py
+++ tests/test_stop_watch.py
@@ -13,27 +13,29 @@ class MyTestCase(unittest.TestCase):
         self.stop_watch = StopWatch(None)
 
     def test_stop_before_start(self):
-        self.assertEqual(True, True)
+        """
+        Should raise error when running stop before start
+        :return:
+        """
         self.assertRaises(AssertionError, self.stop_watch.stop)
 
     def test_reset_before_start(self):
-        self.assertEqual(True, True)
+        """
+        Should raise error when running reset before start
+        """
         self.assertRaises(AssertionError, self.stop_watch.reset)
 
     def test_reset_after_reset(self):
-        self.assertEqual(True, True)
+        """
+        Should allow reset again after reset
+        """
         self.stop_watch.start()
         self.stop_watch.reset()
-        self.assertRaises(AssertionError, self.stop_watch.reset)
+        self.stop_watch.reset()
 
     def test_normal_flow(self):
         """
-        Cannot stop before start
-        Start
-        Stop
-        Reset
-        Does not throw any error
-        :return:
+        Elapsed time should almost match the expected time
         """
         elapsed_time_expected = .200
         self.test_stop_before_start()
@@ -44,6 +46,22 @@ class MyTestCase(unittest.TestCase):
             get_seconds_from_nanoseconds(self.stop_watch.get_elapsed_time()),
             elapsed_time_expected, places=2)  # replace with elapsed time
         self.stop_watch.reset()
+
+    def test_lap_times_recorded_correctly(self):
+        """
+        Recording 3 laps should reflect 3 lap times stored
+        """
+        self.stop_watch.start()
+        time.sleep(.05)
+        self.stop_watch.lap_time()
+        time.sleep(.05)
+        self.stop_watch.lap_time()
+        time.sleep(.05)
+        self.stop_watch.lap_time()
+        self.stop_watch.stop()
+        self.assertEquals(3, len(self.stop_watch.get_lap_times()))
+
+        pass
 
 
 if __name__ == '__main__':
